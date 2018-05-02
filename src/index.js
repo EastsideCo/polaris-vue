c1544d94b6ed3c570efa9d5a931f95e338e322c1
Jonathan Tey <jontey88@gmail.com>
2018-05-02 09:48:03 -0700

    Update index.js
    Hide logging for polaris components in production

--- src/index.js
+++ src/index.js
@@ -202,7 +202,7 @@ export default {
             var componentName = ComponentHelpers.getComponentName(polarisName);
             Vue.component(componentName, components[polarisName]);
             count++;
-            console.log(componentName + ' -> ' + polarisName);
+            log.send(log.DEBUG, TAG, 'componentName + ' -> ' + polarisName');
         }
 
         log.send(log.DEBUG, TAG, 'Installed '+count+' components.');
